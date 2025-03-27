0e841b7b82580cf40015b9a9b83fda84eb9abb16
_Chenbing <muzi@muzilib.com>
2025-03-27 14:56:01 +0800

    fix(model): 修复 emoji 匹配不完整问题
    - 将 matcher.replaceFirst("") 修改为 matcher.replaceAll("")，以移除所有匹配的 emoji
    - 确保在正则替换前统一处理换行符

--- src/main/java/com/c301/plugin/model/GitCommitDomain.java
+++ src/main/java/com/c301/plugin/model/GitCommitDomain.java
@@ -103,7 +103,7 @@ public class GitCommitDomain {
                 // 去除第一个emoji
                 var pattern = Pattern.compile("[\\x{1F300}-\\x{1F5FF}\\x{1F600}-\\x{1F64F}\\x{1F680}-\\x{1F6FF}\\x{2600}-\\x{26FF}\\x{2700}-\\x{27BF}\\x{FE00}-\\x{FE0F}]", Pattern.UNICODE_CHARACTER_CLASS);
                 var matcher = pattern.matcher(rawMessage);
-                if (matcher.find()) rawMessage = matcher.replaceFirst("");
+                if (matcher.find()) rawMessage = matcher.replaceAll("");
 
                 // 在正则前统一处理换行符
                 rawMessage = rawMessage.replaceAll("\\r\\n?", "\n");
